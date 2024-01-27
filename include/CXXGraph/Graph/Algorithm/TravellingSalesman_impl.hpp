cf59eab2e9dd1caac9ecc38babbc1909f03a7aef
Jasmine <tanghocle456@gmail.com>
2024-01-26 20:54:45 -0800

    Setting up implemenetation file

---
+++ include/CXXGraph/Graph/Algorithm/TravellingSalesman_impl.hpp
@@ -0,0 +1,30 @@
+/***********************************************************/
+/***      ______  ____  ______                 _         ***/
+/***     / ___\ \/ /\ \/ / ___|_ __ __ _ _ __ | |__	     ***/
+/***    | |    \  /  \  / |  _| '__/ _` | '_ \| '_ \	 ***/
+/***    | |___ /  \  /  \ |_| | | | (_| | |_) | | | |    ***/
+/***     \____/_/\_\/_/\_\____|_|  \__,_| .__/|_| |_|    ***/
+/***                                    |_|			     ***/
+/***********************************************************/
+/***     Header-Only C++ Library for Graph			     ***/
+/***	 Representation and Algorithms				     ***/
+/***********************************************************/
+/***     Author: ZigRazor ***/
+/***	 E-Mail: zigrazor@gmail.com 				     ***/
+/***********************************************************/
+/***	 Collaboration: Jasmine Tang				     ***/
+/***     Email: thisisjjasmine@gmail.com                             ***/
+/***********************************************************/
+/***	 License: AGPL v3.0 ***/
+/***********************************************************/
+
+#ifndef CXXGRAPH_TRAVELLINGSALESMAN_IMPL_H__
+#define CXXGRAPH_TRAVELLINGSALESMAN_IMPL_H__
+
+#pragma once
+#include "CXXGraph/Graph/Graph_decl.h"
+
+namespace CXXGraph {
+
+}
+#endif CXXGRAPH_TRAVELLINGSALESMAN_IMPL_H__
